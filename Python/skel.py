26b34ca7e3668bf3e90103910987d01480e74f5f
Tyler Bennett <tyler.bennett@availity.com>
2019-06-19 08:28:10 -0500

    commited a basic skeleton

---
+++ Python/skel.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+
+""" Put a description here """
+
+from __future__ import print_function
+from __future__ import absolute_import
+
+## Standard Libraries
+import os
+import sys
+import argparse
+from pprint import pformat
+from pprint import pprint
+
+# Third Party libraries
+import six
+
+# Modules
+
+def main():
+    pass
+
+if __name__=="__main__":
+    main()
