967c0afc55d8b27e661dd9342f92e8794b79aca4
Tyler Bennett <tyler.bennett@availity.com>
2019-08-02 16:39:12 -0500

    commit init commit manifest

---
+++ Python/sample-project/PKG/classes/Manifest.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+from __future__ import print_function
+from __future__ import absolute_import
+
+## Standard Libraries
+import os
+import sys
+from pprint import pprint,pformat
+from configparser import ConfigParser as cfgparser
+
+## Modules
+from .BuildingBlocks import State               #pylint: disable=relative-beyond-top-level
+from .BuildingBlocks import BaseObject          #pylint: disable=relative-beyond-top-level
+
+class Manifest(BaseObject):
+
+    def __init__(self, file=None):
+        if file == None:
+            raise AssertionError("Unable to successfully instantiate object of class::"+self.__class__.__name__)
+
+        self.config_file = file
+        config = self.config_parser = cfgparser()
+        config.read(file)
+
+        if 'qradar' in config:
+            self.setReferenceSetName(config['qradar']['reference_set_name'])
+        if 'ldap' in config:
+            self.setLDAPQueryName(config['ldap']['query_name'])
+            self.setLDAPQueryDefinition(config['ldap']['query_definition'])
+
+        self.validate()
+        self.ready(throw=True)
+
+    def validate(self):
+        self.isValid = False
+        if self.getLDAPQueryName() == None:
+            return False
+        if self.getLDAPQueryDefinition() == None:
+            return False
+        self.isValid = True
+        return True
+
+    def setLDAPQueryName(self, val):
+        self.ldap_query_name = val
+
+    def setLDAPQueryDefinition(self, val):
+        self.ldap_query_definition = val
+
+    def getLDAPQueryName(self):
+        try:
+            return self.ldap_query_name
+        except AttributeError:
+            return None
+
+    def getLDAPQueryDefinition(self):
+        try:
+            return self.ldap_query_definition
+        except AttributeError:
+            return None
+
+    # The standard tuple is returned as such
+    # ldap_named_search is mapped to ldap_query
+    def getTuple(self):
+        return(self.ldap_query_name, self.ldap_query_definition)
+
+    def getTupleViaOrder(self, x, y):
+        import copy
+
+        ## Define acceptable values
+        acceptable_query_names = ['name', 'query_name', 'ldap_query_name']
+        acceptable_query_definitions = ['query', 'query_definition', 'ldap_query_definition', 'ldap_query']
+        acceptable = acceptable_query_names+acceptable_query_definitions
+
+        ## Create two dicts for tracking
+        validation = {
+            "getLDAPQueryName": acceptable_query_names,
+            "getLDAPQueryDefinition": acceptable_query_definitions,
+        }
+        spare = copy.deepcopy(validation)
+
+        # valN is used to return the tuple
+        val1 = val2 = None
+
+        # Validate x variable
+        for fun,lis in validation.items():
+            if x in lis:
+                val1 = getattr(self, fun)()
+                del spare[fun]
+        if val1 == None:
+            raise ValueError("x was not an appropriate value: use one of %s" % str(acceptable) )
+        validation = copy.deepcopy(spare)
+
+        # Validate y variable
+        for fun,lis in validation.items():
+            if y in lis:
+                val2 = getattr(self, fun)()
+                del spare[fun]
+        if val2 == None:
+            raise ValueError("y was not an appropriate value: use one of %s" % str(acceptable) )
+        return (val1, val2)
--- Python/sample-project/PKG/main.py
+++ Python/sample-project/PKG/main.py
@@ -17,9 +17,48 @@ from pprint import pprint,pformat
 
 # Modules
 from classes.BuildingBlocks import State                    #pylint: disable=import-error,no-name-in-module
+from classes.Manifest import Manifest                       #pylint: disable=import-error,no-name-in-module
+
+def init():
+    ## import program state
+    pg_state = State()
+
+    #region BuildArgParser
+    parser = argparse.ArgumentParser(description=__code_desc__)
+    parser.add_argument('-V','--version', action='version', version='%(prog)s '+__code_version__)
+    parser.add_argument('-v','--verbose', action='count', default=0, help="Print verbose output to the console. Multiple v's increase verbosity")
+    parser.add_argument('--debug', action='store_true', help="Toggle debugging output to the console.")
+    args = parser.parse_args()
+    pg_state.args = args
+    #endregion
+
+    # Build some manifests
+    pg_state.manifests = []
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    man_path = os.path.join(dir_path, "manifests")
+    for file in os.listdir(man_path):
+        if file.endswith(".ini"):
+            pg_state.manifests.append(Manifest(os.path.join(man_path,file)))
+
+    #region InitLDAP
+    if args.verbose:
+        print("[*] Setting LDAP parameters")
+    pg_state.domain_name = "google.com"
+    pg_state.ldap_root = "DC=google,DC=com"
+    pg_state.bind_dn = "CN=ldapbind,OU=Service Accounts,DC=google,DC=com"
+    pg_state.bind_pass = None #pylint: disable=no-member
+    if args.verbose > 1:
+        print("[**] Set pg_state.domain_name: %s" % (str(pg_state.domain_name)))
+        print("[**] Set pg_state.ldap_root: %s" % (str(pg_state.ldap_root)))
+        print("[**] Set pg_state.bind_dn: %s" % (str(pg_state.bind_dn)))
+        print("[**] Fetch/Set pg_state.bind_pass: %s" % ("<redacted>"))
+    #endregion
 
 def main():
-    pass
+    init()
+    pg_state = State()
+    args = pg_state.args
+
 
 
 if __name__=="__main__":
