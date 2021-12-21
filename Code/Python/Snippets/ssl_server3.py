e20e54d9235507c36326fde248de13e5284a895c
Tyler Bennett <tbennett6421@gmail.com>
2021-12-20 23:29:35 -0600

    add try-catch block

--- Code/Python/Snippets/ssl_server3.py
+++ Code/Python/Snippets/ssl_server3.py
@@ -12,18 +12,26 @@ except ImportError:
     elif v == '2':
         six.PY2 = True
 
-import BaseHTTPServer, SimpleHTTPServer
-import ssl
+if six.PY2:
+    import BaseHTTPServer, SimpleHTTPServer
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
 
+import ssl
 keyfile='/etc/ssl/private/snakeoil.key'
 certfile='/etc/ssl/certificates/snakeoil.pem'
 
-httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', 443),
-        SimpleHTTPServer.SimpleHTTPRequestHandler)
-
-httpd.socket = ssl.wrap_socket (httpd.socket,
+ip = '0.0.0.0'
+port = 8443
+print("[*] Opening socket: %s:%s" % (ip, port))
+httpd = BaseHTTPServer.HTTPServer( (ip, port),
+            SimpleHTTPServer.SimpleHTTPRequestHandler
+        )
+try:
+    httpd.socket = ssl.wrap_socket (httpd.socket,
         keyfile=keyfile,
         certfile=certfile,
-        server_side=True)
-
-httpd.serve_forever()
+        server_side=True
+    )
+    httpd.serve_forever()
+except IOError:
+    print("[!]: Unable to open TLS files: (%s)" % [keyfile, certfile])
