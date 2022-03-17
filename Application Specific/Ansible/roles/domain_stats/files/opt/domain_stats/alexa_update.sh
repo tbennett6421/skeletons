51e4f7aa97c0a39fb2697c36953b07f227a94c64
Tyler Bennett <tyler.bennett@availity.com>
2022-03-17 16:52:39 -0500

    use curl over wget for osx

--- Application Specific/Ansible/roles/domain_stats/files/opt/domain_stats/alexa_update.sh
+++ Application Specific/Ansible/roles/domain_stats/files/opt/domain_stats/alexa_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -f top-1m-alexa.csv.zip
-/usr/bin/wget http://s3.amazonaws.com/alexa-static/top-1m.csv.zip -O top-1m-alexa.csv.zip
+/usr/bin/curl http://s3.amazonaws.com/alexa-static/top-1m.csv.zip > top-1m-alexa.csv.zip
 /usr/bin/unzip -o top-1m-alexa.csv.zip
 mv top-1m.csv top-1m-alexa.csv
 rm -f top-1m-alexa.csv.zip
