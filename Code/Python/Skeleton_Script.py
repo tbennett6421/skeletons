f141a4c36efb81ddd178c3c59eda37a78ab64a3e
Tyler Bennett <tyler.bennett@availity.com>
2022-10-13 10:05:01 -0500

    update skeleton

--- Code/Python/Skeleton_Script.py
+++ Code/Python/Skeleton_Script.py
@@ -1,24 +1,40 @@
+from __future__ import (print_function,absolute_import)
+
 __code_desc__ = "Put a description here"
-__code_version__ = 'v0.0.0'
+__code_version__ = 'v0.0.1'
 __code_debug__ = False
 __code_color_support__ = True
+__code_vsc_support__ = True
 
 ## Standard Libraries
 import os
 import sys
 import logging
-import argparse
+from argparse import ArgumentParser, RawTextHelpFormatter
+from pprint import pformat, pprint
 
-# Third Party libraries
-if __code_color_support__:
-    try:
-        import colorama
-        from termcolor import colored
-    except ImportError:
-        __code_color_support__ = False
+## Third Party libraries
+try:
+    # Do we have what we need to use colors?
+    import colorama
+    from termcolor import colored
+    __code_color_support__ = True
+except ImportError:
+    __code_color_support__ = False
+
+try:
+    # Do we have what we need to work with vscode
+    import debugpy
+    debugpy.listen(5678)
+    debugpy.wait_for_client()
+    __code_vsc_support__ = True
+except (ImportError, SystemExit, RuntimeError) as e:
+    __code_vsc_support__ = False
 
 # Modules
+# place content here
 
+# Local functions
 def print_console(msg, marker="[?]", fg_color=None, bg_color=None):
     f_str = f"{marker} {msg}"
     if __code_color_support__:
@@ -27,22 +43,21 @@ def print_console(msg, marker="[?]", fg_color=None, bg_color=None):
         print(f_str)
 
 def print_verbose(msg, marker='[*]'):
-    print_console(msg,marker, fg_color='cyan')
+    print_console(msg, marker, fg_color='cyan')
 
 def print_info(msg, marker='[+]'):
-    print_console(msg,marker, fg_color='green')
+    print_console(msg, marker, fg_color='green')
 
 def print_error(msg, marker='[!]'):
-    print_console(msg,marker, fg_color='white', bg_color='on_red')
+    print_console(msg, marker, fg_color='white', bg_color='on_red')
 
 def init_colors(b=True):
     if b:
         colorama.init()
     else:
-        global __code_color_support__
         __code_color_support__ = False
 
-def begin_logging():
+def begin_logging(debug=False):
     handler = logging.StreamHandler()
     handler.setFormatter(
         logging.Formatter(
@@ -59,10 +74,11 @@ def begin_logging():
     return log
 
 def collect_args():
-    parser = argparse.ArgumentParser(description=__code_desc__,
-        formatter_class=argparse.RawTextHelpFormatter)
+    parser = ArgumentParser(description=__code_desc__,
+        formatter_class=RawTextHelpFormatter)
     parser.add_argument('-V', '--version', action='version', version=__code_version__)
     parser.add_argument('-v', '--verbose', action='count', default=0)
+    parser.add_argument('--debug', action='store_true', help="Enable debugging")
     parser.add_argument('--disable-color', action='store_true',
         help='disable colorized output (you monster))')
     args = parser.parse_args()
@@ -72,14 +88,17 @@ def handle_args():
     # collect parser if needed to conditionally call usage: parser.print_help()
     parser, args = collect_args()
 
+    if args.debug:
+        __code_debug__ = True
+
     # negate args.disable_color; init_colors if needed
     init_colors(not args.disable_color)
 
     return parser, args
 
 def main():
-    log = begin_logging()
     parser, args = handle_args()
+    log = begin_logging(debug=__code_debug__)
 
 if __name__=="__main__":
     main()
